82226736f530c945e7636f361b0430e7c3ae64ab
Idriss Atik <idriss.atik@LSTKCO108260.local>
2024-01-10 13:03:06 +0100

    add framer-motion

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,20 +1,25 @@
+'use client';
+
 import './navBar.scss';
+import { motion } from 'framer-motion';
+import { slideInFromLeft, slideInFromRight } from '../../utils/motion.js';
 
 function NavBar() {
   return (
-    <div className="navbar">
+    <motion.div initial="hidden" animate="visible" className="navbar">
       <div className="wrapper">
-        <span>Idriss Dev</span>
-        <div className="social">
+        <motion.span variants={slideInFromLeft(0.5)}>Idriss Dev</motion.span>
+
+        <motion.div variants={slideInFromRight(0.5)} className="social">
           <a href="">
             <img src="/github.svg" alt="github icon" />
           </a>
           <a href="">
             <img src="/linkedin.svg" alt="linkedin icon" />
           </a>
-        </div>
+        </motion.div>
       </div>
-    </div>
+    </motion.div>
   );
 }
 
