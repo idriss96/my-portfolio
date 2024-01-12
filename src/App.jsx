45b66c1890b1e9ceafcd1081be1d4f505056c238
Idriss Atik <idriss.atik@LSTKCO108260.local>
2024-01-12 10:59:07 +0100

    add hero-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './app.scss';
 import NavBar from './components/navBar/NavBar';
+import Hero from './components/hero/Hero'
 const App = () => {
   return (
     <div>
       <section id="Homepage">
         <NavBar />
+        <Hero />
       </section>
       <section>Parallax</section>
       <section id="Skills">Skills</section>
